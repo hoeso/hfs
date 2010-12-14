eda872e1d45d101ac325542c232d420692fbea6f
Hans Hoermann <support@hans-hoermann.de>
2010-12-14 17:36:23 +0100

    Anlegen von Verbindungsentitaeten portiert.

---
+++ containerHinweis.php
@@ -0,0 +1,6 @@
+<h3>
+<?
+echo $_eHinweis[0]; // 1. Hinweis anzeigen
+array_splice($_eHinweis, 0, 1); // zum naechsten Hinweis schalten
+?>
+</h3>
--- mn.php
+++ mn.php
@@ -1,5 +1,5 @@
 <?
-if( 1 )
+if( 0 )
 {
   echo"\nPOSTS:<br>";
   foreach( $_POST as $k=>$elem )
