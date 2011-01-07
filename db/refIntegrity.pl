5bc116f2255bcd85b5f618c20fbefd841b7b27a7
Hans Hoermann <support@hans-hoermann.de>
2011-01-07 19:51:49 +0100

    1 zu N speichern, generieren von refIntegrity.prj mit leaf-Markierung

--- db/refIntegrity.pl
+++ db/refIntegrity.pl
@@ -15,25 +15,28 @@ foreach (@t)
   #print "dbg: $_\n";
 }
 
+# Vektor eroeffnen ...
+print "<?\n\$arrRefs = array(\n";
 foreach (@a)
 {
   if ( /DROP TABLE IF EXISTS `(.*)`/ )
   {
     $w = $1;
+    # diese Entitaet ist dran zum Pruefen ...
+    # Logbuch leeren ...
+    %log = ();
+    print "\n  \"$w\"=>array(\n";
     foreach (@t)
     {
   #print "\ndbg: $_\n";
       if( /$w/ )
-      { # diese Entitaet ist dran zum Pruefen ...
-    print "\ndbg1: $w\n";
+      {
         foreach (@a)
         {
           if ( /CREATE TABLE `(.*)`/ )
           {
             $e = $1;
 	    $fk = $w . "ID";
-    print "\ndbg2: $fk\n";
-	    #print "$e pruefen auf $fk\n";
 	  }
 	  elsif ( $fk && /UNIQUE KEY.*$fk/ )
 	  {
@@ -41,13 +44,31 @@ foreach (@a)
 	  }
 	  elsif ( $fk && /$fk/ )
 	  {
-            print " ... $fk gefunden in $e\n";
+	    if ( $log{$e} )
+	    {
+	    }
+	    else
+	    {
+              print "    \"$e\",\n";
+	      $log{$e}="1";
+	    }
 	  }
 	}
       }
     }
+    # diese Entitaet ist fertig mit geprueft werden.
+    if ( %log )
+    {
+    }
+    else
+    {
+      print "    \"1-leaf\",\n";
+    }
+    print "  ),\n";
   }
 }
+# Vektor schliessen ...
+print "\n);\n?>";
 #$paerchen =~ s/jp=.&//;
 #$paerchen .= "_sepperl=manuell001";
 
