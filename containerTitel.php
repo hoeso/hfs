f63fac904dcd88e2230224068d80041fe4dd58b9
Hans Hoermann <support@hans-hoermann.de>
2013-02-06 22:08:18 +0100

    Wrapper fuer alle moeglichen FK-Entitaeten geschrieben, PHP-Notices entfernt

--- containerTitel.php
+++ containerTitel.php
@@ -1,6 +1,9 @@
 <h2>
 <?
-echo $_eTitel[0]; // 1. Titel anzeigen
-array_splice($_eTitel, 0, 1); // zum naechsten Titel schalten
+if( isset($_eTitel[0]) )
+{
+  echo $_eTitel[0]; // 1. Titel anzeigen
+  array_splice($_eTitel, 0, 1); // zum naechsten Titel schalten
+}
 ?>
 </h2>
