38e6b1afbe55a20b4d354c95f3c3425bfcf12f31
Hans@Assistance21 <support@hans-hoermann.de>
2013-09-04 19:32:04 +0200

    LVView gibt Spalten-Beschriftungen aus und den Zelleninhalt der 1. Spalte auf allen gepufferten Zeilen.

--- LVView.php
+++ LVView.php
@@ -2,8 +2,11 @@
 require_once("LVModel.php");
 class LVView extends LVModel
 {
-  function __construct($d, $thema=1, $czeile=0, $fRec=0, $mode="r")
+  private $lvID;
+
+  function __construct($lvID, $d, $thema=1, $czeile=0, $fRec=0, $mode="r")
   {
+    $this->lvID = $lvID;
     parent::__construct($d, $thema, $czeile, $fRec, $mode);
   }
   function __get($var)
@@ -38,7 +41,40 @@ class LVView extends LVModel
       return;
     }
   }
-  function display( $lvid )
+  function display()
   {
+    $aS[0]=0;// hier Feld 0 rein = ID
+    $aS[1]=1;// hier Feld 1 rein = Titel
+    $dimS = count($aS);
+    $sl = "SELECT ID, Titel FROM Spalte WHERE " . $this->lvID . "=LVID ORDER BY Nr";
+    DB::gibFelderArray( $sl, $aS );
+    echo "\n";?><table><?
+    /*** Spalten-Beschriftung anzeigen                           ***/
+    echo "\n";?><tr><?
+    for( $s=0; $s < count($aS); $s += $dimS )
+    {
+      echo "\n";?><th><?
+      echo $aS[$s+1];
+      ?></th><?echo "\n";
+    }
+    ?></tr><?echo "\n";
+    $aZ[0]=0;// hier Feld 0 rein = ID
+    $aZ[1]=1;// hier Feld 1 rein = Nr
+    $dimZ = count($aZ);
+    $sl = "SELECT ID, Nr FROM Zeile WHERE " . $this->lvID . "=LVID ORDER BY Nr";
+    DB::gibFelderArray( $sl, $aZ );
+    /*** Der Reihe nach die Zeilen ausgeben                      ***/
+    for( $z=0; $z < count($aZ); $z += $dimZ )
+    {
+      echo "\n";?><tr><?
+      for( $s=0; $s < count($aS); $s += $dimS )
+      {
+        ?><td><?
+        echo Db::gibFeld( "SELECT Inhalt FROM Zelle c JOIN Spalte s ON (c. SpalteID=s.ID) JOIN Zeile z ON (c. ZeileID=z.ID) WHERE " . $aS[$s] . "=s.ID AND " . $aZ[$z] . "=z.ID" );
+        ?></td><?
+      }
+      ?></tr><?echo "\n";
+    }
+    echo "\n";?></table><?
   }
 }
