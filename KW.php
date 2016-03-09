ac08d9067efc43f5f6d93e16a92efa3ae6f6de9f
Hans <support@hans-hoermann.de>
2016-03-09 15:40:40 +0100

    Start ermitteln Wochentage mit Datum.

--- KW.php
+++ KW.php
@@ -3,12 +3,26 @@ class KW extends DateTime
 {
   private $ID;
   private $KW;
+  private $wochenTag;
   private $zeit;
+  private $tag;
+  private $datum;
 
   function __construct($time)
   {
     date_default_timezone_set('Europe/Berlin');
+    $this->tag = array (
+      "So",
+      "Mo",
+      "Di",
+      "Mi",
+      "Do",
+      "Fr",
+      "Sa"
+    );
     $this->KW = date( "W" );
+    $this->wochenTag = date( "w" );
+    $this->datum = date( "j.n." );
     $this->zeit = $time;
     //parent::__construct($time);
     parent::__construct();
@@ -17,12 +31,14 @@ class KW extends DateTime
   {
     switch($var)
     {
+      case 'Datum':
+        return $this->datum;
       case 'Kalenderwoche':
         return $this->KW;
+      case 'Wochentag':
+        return $this->tag[$this->wochenTag];
       case 'eilt':
         //return parent::__get('eilt');
-      case 'Nummer':
-        return $this->Nummer;
       default:
         throw new Exception("KW hat keine Eigenschaft $var.", 1 );
       break;
