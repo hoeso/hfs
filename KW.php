3990eaaf4b2ed6d50f88cb02b5b8c1f7194875cf
Hans Hoermann <info@hoermann-solutions.com>
2016-03-10 21:41:26 +0100

    Aktuelle KW: assoziativer Vektor kann mit Datum versehen und ausgegeben werden.

--- KW.php
+++ KW.php
@@ -1,15 +1,11 @@
 <?php
-class KW extends DateTime
+class KW
 {
-  private $ID;
   private $KW;
   private $wochenTag;
-  private $zeit;
   private $tag;
-  private $datum;
-  private $heute;
 
-  function __construct($time)
+  function __construct()
   {
     date_default_timezone_set('Europe/Berlin');
     $this->tag = array (
@@ -21,42 +17,38 @@ class KW extends DateTime
       "Fr" => "",
       "Sa" => ""
     );
-    var_dump($this->tag); echo "\n<br>";
+    //var_dump($this->tag); echo "\n<br>";
     $this->KW = date( "W" );
     $this->wochenTag = date( "w" );
-    $this->datum = date( "j.n." );
-    $this->heute = date( "Y-m-d" );
-    $this->zeit = $time;
     $c = 0;
     foreach ($this->tag as $key => &$value)
     {
+      $datum = new DateTime(date( "Y-m-d" ));
       if( $c == $this->wochenTag )
       {
-        $value = $this->datum;
-	$this->tag["$key"] = $this->datum;
-	break;
+        $value = $datum->format("j.n.");
+      }
+      else if( $c < $this->wochenTag )
+      {
+        
+        date_sub($datum,date_interval_create_from_date_string( $this->wochenTag - $c . " days" ));
+        $value = $datum->format("j.n."); 
+      }
+      else if( $c > $this->wochenTag )
+      {
+        
+        date_add($datum,date_interval_create_from_date_string( $c - $this->wochenTag . " days" ));
+        $value = $datum->format("j.n."); 
       }
       ++$c;
     }
-    var_dump($this->tag); echo "\n<br>";
-    echo $this->tag['Do'] . "<br>";
-    //$datum = new DateTime($this->heute);
-    $datum = new DateTime(date( "Y-m-d" ));
-    $gestern = new DateTime(date( "Y-m-d" ));
-    date_sub($gestern,date_interval_create_from_date_string("1 days"));
-    //$datum = parent::sub($this->heute,"1 day");
-
-    echo "heute: " . $datum->format("j.n.") . "; gestern: " . $gestern->format("j.n.");
-    //echo "heute: " . $this->heute
-    //parent::__construct($time);
-    parent::__construct();
+    foreach ($this->tag as $key => &$value)
+      echo $key . " " . $value . "    ";
   }
   function __get($var)
   {
     switch($var)
     {
-      case 'Datum':
-        return $this->datum;
       case 'Kalenderwoche':
         return $this->KW;
       case 'Wochentag':
