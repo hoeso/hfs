d6a8037f10dbafc63ac132a26cd4fbcfb164bde2
Hans Hoermann <info@hoermann-solutions.com>
2016-03-14 20:43:05 +0100

    KW mit den geplanten Uhrzeiten +- 1 h anzeigen.

--- KWmodel.php
+++ KWmodel.php
@@ -1,15 +1,20 @@
 <?php
+require_once("Db.php");
 require_once("KW.php");
 class KWmodel extends KW
 {
   static protected $db; // db->db : DB-Handle
   protected $tag;
+  private   $go;   // fruehester Betreuungsbeginn
+  private   $stop; // spaetestes Betreuungsende
 
   function __construct($strDate)
   {
+    $this->db = Db::getInstance();
+    $this->go = DB::gibFeld( "SELECT IF(4 < MIN(v.ID), MIN(v.ID)-4, 1) FROM VS v JOIN ClientVS cv ON (v.ID=cv.VSID) JOIN MAClientVS mcv ON (cv.ID=mcv.ClientVSID)", 0 );
+    $this->stop = DB::gibFeld( "SELECT IF(v.ID + cv.Menge > 96, 96, v.ID + cv.Menge) FROM VS v JOIN ClientVS cv ON (v.ID=cv.VSID) JOIN MAClientVS mcv ON (cv.ID=mcv.ClientVSID) ORDER BY v.ID DESC LIMIT 1", 0 );
     parent::__construct($strDate);
     //var_dump($this->tag); echo "\n<br>";
-    $this->db = Db::getInstance();
   }
   function __get($var)
   {
@@ -17,16 +22,19 @@ class KWmodel extends KW
     {
       case 'Datum':
         return parent::__get('Datum');
+      case 'Go':
+        return $this->go;
       case 'Kalenderwoche':
         return parent::__get('Kalenderwoche');
       case 'KWweiter':
         return parent::__get('KWweiter');
       case 'KWzurueck':
         return parent::__get('KWzurueck');
+      case 'Stop':
+        return $this->stop;
       default:
         throw new Exception("KWmodel hat keine Eigenschaft $var.", 1 );
       break;
     }
   }
 }
-
--- KWview.php
+++ KWview.php
@@ -1,5 +1,6 @@
 <?php
 require_once("KWmodel.php");
+include("vektorQuart.prj"); // $quart
 class KWview extends KWmodel
 {
   protected $tag;
@@ -15,12 +16,16 @@ class KWview extends KWmodel
     {
       case 'Datum':
         return parent::__get('Datum');
+      case 'Go':
+        return parent::__get('Go');
       case 'Kalenderwoche':
         return parent::__get('Kalenderwoche');
       case 'KWweiter':
         return parent::__get('KWweiter');
       case 'KWzurueck':
         return parent::__get('KWzurueck');
+      case 'Stop':
+        return parent::__get('Stop');
       default:
         throw new Exception("KWview hat keine Eigenschaft $var.", 1 );
       break;
@@ -28,12 +33,45 @@ class KWview extends KWmodel
   }
   function show()
   {
+    global $quart;
+    ?><table><tr><?php
+    /*** 1. Header ausgeben           ***/
     $i=0;
     foreach ($this->tag as $key => $value)
     {
-      if( $i )
-        echo $key . " " . $value . "    ";
+      if( !$i )
+      {?>
+        <th><a href="">-----</a></th><?php
+        ++$i;
+	continue;
+      }?>
+      <th><?php
+      echo $key . " " . $value . "    ";
+      ?></th><?php
       ++$i;
-    }
+    }?>
+    </tr><tr><?php
+    /*** 2. Quart-Zeilen ausgeben     ***/
+    $row = $this->Go;
+    while( $row < $this->Stop )
+    {
+      $i=0;
+      foreach ($this->tag as $key => $value)
+      {
+        if( !$i )
+        {?>
+          <td><?php echo $quart[$row];?></td><?php
+          ++$i;
+	  continue;
+        }?>
+        <td><?php
+        echo $key;
+        ?></td><?php
+        ++$i;
+      }?>
+      </tr><tr><?php
+      ++$row;
+    }?>
+    </tr></table><?php
   }
 }
