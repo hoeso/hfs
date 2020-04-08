42dacc18ca3fc3bb80586cfce34b724dc5939af1
Hans <support@hans-hoermann.de>
2020-04-08 14:02:33 +0200

    Plan, Nachschulung: Vergangene 4 Wochen anzeigen.

--- KWview.php
+++ KWview.php
@@ -279,7 +279,7 @@ class KWview extends KWmodel
 	    $d="&d";
 	  else
 	    $d="";
-          ?><a href="mn.php?mn=planend&a=Termin&sl=5&sl1=Leistung&sl2=Trainer&sl3=Ort&sl4=<?php echo $row;?>&sl5=<?php echo $i;?>&navi=Plan&u=KW<?php echo $this->Kalenderwoche;?>&k=<?php echo $this->Kalenderwoche;?>&j=<?php echo $this->Jahr . $d;?>&planungVS_x&i=6" target="_blank" title="<?php echo $dayofweek . " " . $quart[$row];?>">&nbsp;</a>&nbsp;<?php
+          ?><a href="mn.php?mn=planend&a=Termin&sl=5&sl1=Leistung&sl2=Trainer&sl3=Ort&sl4=<?php echo $row;?>&sl5=<?php echo $i;?>&navi=Plan&u=KW<?php echo $this->Kalenderwoche;?>&k=<?php echo $this->Kalenderwoche;?>&j=<?php echo $this->Jahr . $d;?>&planungVS_x&i=3" target="_blank" title="<?php echo $dayofweek . " " . $quart[$row];?>">&nbsp;</a>&nbsp;<?php
 	  /***
           if( isset($clutch) && 1 < $i ) // 1 < $i: Vorgaenger 'Sonntag' weglassen
           {
@@ -303,7 +303,7 @@ class KWview extends KWmodel
 	    $d="&d";
 	  else
 	    $d="";
-          ?><a href="mn.php?mn=planend&a=Termin&sl=5&sl1=Leistung&sl2=Trainer&sl3=Ort&sl2ID=<?php echo $a[6]?>&sl4=<?php echo $row;?>&sl5=<?php echo $i;?>&navi=Plan&u=KW<?php echo $this->Kalenderwoche;?>&k=<?php echo $this->Kalenderwoche;?>&j=<?php echo $this->Jahr . $d;?>&planungVS_x&i=6" target="_blank" title="<?php echo $dayofweek . " " . $quart[$row];?>">+&nbsp;</a>&nbsp;<?php
+          ?><a href="mn.php?mn=planend&a=Termin&sl=5&sl1=Leistung&sl2=Trainer&sl3=Ort&sl2ID=<?php echo $a[6]?>&sl4=<?php echo $row;?>&sl5=<?php echo $i;?>&navi=Plan&u=KW<?php echo $this->Kalenderwoche;?>&k=<?php echo $this->Kalenderwoche;?>&j=<?php echo $this->Jahr . $d;?>&planungVS_x&i=3" target="_blank" title="<?php echo $dayofweek . " " . $quart[$row];?>">+&nbsp;</a>&nbsp;<?php
 	  for( $k=0; $k < count($a); $k += $dim )
 	  {
             $maclientvsID=$a[$k+4];
