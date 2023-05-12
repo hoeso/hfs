078a5cc101597901b42182cfe16bc5f6d527da00
alex <devkral@web.de>
2023-05-12 12:07:29 +0200

    fix index.php

--- public_html/index.php
+++ public_html/index.php
@@ -1,19 +1,3 @@
-<!DOCTYPE html>
-<html>
-
-<head>
-  <meta http-equiv="content-type" content="text/html;charset=utf-8">
-  <title>HFS-1</title>
-  <script language=javascript type='text/javascript'>
-    if (top.location != self.location) {
-      top.location = self.location
-    }
-  </script>
-  <!--link rel="stylesheet" href="./erica.css" media="screen" title="erica Stylesheet" /-->
-</head>
 <?php
 include("../src/index.php");
 ?>
-</body>
-
-</html>
